8283c34dc855629d51726cedf9f29cb6d57f3c2b
marophius <52643566+marophius@users.noreply.github.com>
2020-07-17 03:32:13 -0300

    PrimeiraLojaVirtual
    Inicio do projeto com lojas virtuais

---
+++ LojaVirtual/LojaVirtual/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using LojaVirtual.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private Product GetProduct()
+        {
+            return new Product()
+            {
+                Id = 1,
+                Name = "Xbox One X",
+                Description = "Jogue em 4k",
+                Price = 2000.00M
+            };
+        }
+
+        public ActionResult Visualizar()
+        {
+            Product product =  GetProduct();
+
+            return View(product);
+        }
+    }
+}
