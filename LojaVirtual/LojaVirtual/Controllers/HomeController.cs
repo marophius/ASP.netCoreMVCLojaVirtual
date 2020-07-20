f610e91ad63cb2d2e21bec3f739d6c6fc274574a
marophius <52643566+marophius@users.noreply.github.com>
2020-07-20 05:47:29 -0300

    Seção 8 concluída
    Fizemos um sistema de envio de contato por email e adicionamos validações no formulário.

--- LojaVirtual/LojaVirtual/Controllers/HomeController.cs
+++ LojaVirtual/LojaVirtual/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using LojaVirtual.Models;
+using LojaVirtual.Libraries.Email;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
 
 namespace LojaVirtual.Controllers
 {
@@ -16,6 +20,61 @@ namespace LojaVirtual.Controllers
         public IActionResult Contato()
         {
             return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult CadastroCliente()
+        {
+            return View();
+        }
+
+        public IActionResult CarrinhoCompras()
+        {
+            return View();
+        }
+
+        public IActionResult ContatoAcao()
+        {
+            try
+            {
+                Contato contato = new Contato();
+                contato.Nome = HttpContext.Request.Form["name"];
+                contato.Email = HttpContext.Request.Form["email"];
+                contato.Texto = HttpContext.Request.Form["texto"];
+
+                var ListaMensagens = new List<ValidationResult>();
+                var contexto = new ValidationContext(contato);
+                bool isValid = Validator.TryValidateObject(contato, contexto, ListaMensagens, true);
+
+                if(isValid)
+                {
+                    ContatoEmail.EnviarContatoEmail(contato);
+
+                    ViewData["MSG_S"] = "Mensagem de contato enviada com sucesso!";
+                }
+                else
+                {
+                    StringBuilder sb = new StringBuilder();
+                    foreach(var text in ListaMensagens)
+                    {
+                        sb.Append(text.ErrorMessage + "<br />");
+                    }
+
+                    ViewData["MSG_E"] = sb.ToString();
+                    ViewData["CONTATO"] = contato;
+                }
+            }
+            catch(Exception e)
+            {
+                ViewData["MSG_E"] = "Ops! Tivemos um erro, tente novamente";
+            }
+            
+
+            return View("Contato");
         }
     }
 }
